68ebddd3321aff9ada2021ea36849b69e6b36438
Dajiang7719-openwrt <326881599@qq.com>
2026-06-07 13:20:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,10 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#  添加OpenAppFilter应用过滤源码
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+#  添加常用NAS/VPN插件源码
+# 这里用的是通用仓库，你可以按需开启
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
